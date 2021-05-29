4dc6e3cefa6a036f384a7c7c87ae2447a6077609
root <root@ip-172-31-13-51.ap-south-1.compute.internal>
2021-05-29 07:29:57 +0000

    shell

---
+++ math.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VAR1=3
+VAR2=4
+
+echo $(( VAR1 + VAR2 ))
+echo $(( VAR1 - VAR2 ))
+echo $(( VAR1 * VAR2 ))
+echo $(( VAR1 / VAR2 ))
+echo $(( VAR1 % VAR2 ))
+echo $(( VAR1 ** VAR2 ))
+echo $(( VAR1 += VAR2 ))
+echo $(( VAR1 -= VAR2 ))
+echo $(( VAR1 *= VAR2 ))
+echo $(( VAR1 /= VAR2 ))
+echo $(( VAR1 = VAR2 ))
---
+++ myfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#purpose: 
+#version: 
+#create date:  Sat May 29 04:08:43 UTC 2021
+#creator:  root
+#start:
+echo "$0"
+echo "$#"
+echo "$*"
+echo "$@"
+echo "$?"
+echo "$$"
+echo "$!"
+#end: 
